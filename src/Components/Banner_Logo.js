f0a39beb52e6f0132b6017e468c926e81ee0a52c
conan213 <74485959+conan213@users.noreply.github.com>
2025-01-15 11:03:43 +0800

    Update Banner_Logo.js

--- src/Components/Banner_Logo.js
+++ src/Components/Banner_Logo.js
@@ -33,25 +33,25 @@ export default function Banner_Logo() {
 <ConsoleLine ascii message="| |  / /___  (_)___/ /_  __/__  _________ ___  (_)___  ____ _/ /   |  /  _/" />
 <ConsoleLine ascii message="| | / / __ \/ / __  / / / / _ \/ ___/ __ `__ \/ / __ \/ __ `/ / /| |  / /  " />
 <ConsoleLine ascii message="| |/ / /_/ / / /_/ / / / /  __/ /  / / / / / / / / / / /_/ / / ___ |_/ /   " />
-<ConsoleLine ascii message="|___/\____/_/\__,_/ /_/  \___/_/  /_/ /_/ /_/_/_/ /_/\__,_/_/_/  |_/___/" />
-<ConsoleLine ascii message="                            :))IIITTTTTLTTTTTTLLHLLLLL);=)II)IIIIi=:" />
-<ConsoleLine ascii message="                            :i)IIITTTTTTTTTLLLHLLHLL)+=)II)ITTTI)i=" />
-<ConsoleLine ascii message="                            .i)IIITTTTITTLLLHHLLLL);=)II)ITTTTII)i+" />
-<ConsoleLine ascii message="                            =i)IIIIIITTLLLLLLHLL=:i)II)TTTTTTIII)i&#39;" />
-<ConsoleLine ascii message="                          +i)i)))IITTLLLLLLLLT=:i)II)TTTTLTTIII)i;" />
-<ConsoleLine ascii message="                        +ii)i:)IITTLLTLLLLT=;+i)I)ITTTTLTTTII))i;" />
-<ConsoleLine ascii message="                       =;)i=:,=)ITTTTLTTI=:i))I)TTTLLLTTTTTII)i;" />
-<ConsoleLine ascii message="                     +i)ii::,  +)IIITI+:+i)I))TTTTLLTTTTTII))=," />
-<ConsoleLine ascii message="                   :=;)i=:,,    ,i++::i))I)ITTTTTTTTTTIIII)=+'" />
-<ConsoleLine ascii message="                 .+ii)i=::,,   ,,::=i)))iIITTTTTTTTIIIII)=+" />
-<ConsoleLine ascii message="                ,==)ii=;:,,,,:::=ii)i)iIIIITIIITIIII))i+:&#39;" />
-<ConsoleLine ascii message="               +=:))i==;:::;=iii)+)=  `:i)))IIIII)ii+'" />
-<ConsoleLine ascii message="             .+=:))iiiiiiii)))+ii;" />
-<ConsoleLine ascii message="            .+=;))iiiiii)));ii+" />
-<ConsoleLine ascii message="           .+=i:)))))))=+ii+" />
-<ConsoleLine ascii message="          .;==i+::::=)i=;" />
-<ConsoleLine ascii message="          ,+==iiiiii+," />
-<ConsoleLine ascii message="          `+=+++;`" />
+<ConsoleLine ascii message="|___/\____/_/\__,_/ /_/  \___/_/  /_/ /_/ /_/_/_/ /_/\__,_/_/_/  |_/___/" /><br/>
+<ConsoleLine ascii message="                   :))IIITTTTTLTTTTTTLLHLLLLL);=)II)IIIIi=:" />
+<ConsoleLine ascii message="                   :i)IIITTTTTTTTTLLLHLLHLL)+=)II)ITTTI)i=" />
+<ConsoleLine ascii message="                   .i)IIITTTTITTLLLHHLLLL);=)II)ITTTTII)i+" />
+<ConsoleLine ascii message="                   =i)IIIIIITTLLLLLLHLL=:i)II)TTTTTTIII)i&#39;" />
+<ConsoleLine ascii message="                  +i)i)))IITTLLLLLLLLT=:i)II)TTTTLTTIII)i;" />
+<ConsoleLine ascii message="                +ii)i:)IITTLLTLLLLT=;+i)I)ITTTTLTTTII))i;" />
+<ConsoleLine ascii message="               =;)i=:,=)ITTTTLTTI=:i))I)TTTLLLTTTTTII)i;" />
+<ConsoleLine ascii message="             +i)ii::,  +)IIITI+:+i)I))TTTTLLTTTTTII))=," />
+<ConsoleLine ascii message="           :=;)i=:,,    ,i++::i))I)ITTTTTTTTTTIIII)=+'" />
+<ConsoleLine ascii message="         .+ii)i=::,,   ,,::=i)))iIITTTTTTTTIIIII)=+" />
+<ConsoleLine ascii message="        ,==)ii=;:,,,,:::=ii)i)iIIIITIIITIIII))i+:&#39;" />
+<ConsoleLine ascii message="       +=:))i==;:::;=iii)+)=  `:i)))IIIII)ii+'" />
+<ConsoleLine ascii message="      .+=:))iiiiiiii)))+ii;" />
+<ConsoleLine ascii message="     .+=;))iiiiii)));ii+" />
+<ConsoleLine ascii message="    .+=i:)))))))=+ii+" />
+<ConsoleLine ascii message="   .;==i+::::=)i=;" />
+<ConsoleLine ascii message="  ,+==iiiiii+," />
+<ConsoleLine ascii message=" `+=+++;`" />
 
             
           </div>
